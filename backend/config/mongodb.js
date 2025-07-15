8ccc6f35ce911a0a4a98a8fc1354ef5f3f52a4b3
bhaskarsingh96 <bhaskarsingh91642@gmail.com>
2025-07-15 23:52:20 +0530

    first commit

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -3,7 +3,7 @@ import mongoose, { mongo } from "mongoose";
 const connectDB = async () => {
     mongoose.connection.on('connected',()=>console.log("Database Connected"));
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/prescripto`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/Syncare`)
 }
 
 export default connectDB;
